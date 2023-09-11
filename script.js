673021c7b8fe7b4fd455ba5af259ad32924ab4a6
Kunal Sharma <131008147+Thatkunal@users.noreply.github.com>
2023-09-11 19:00:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,179 +6,282 @@ var additempopup = document.getElementById("additempopup")
 var notask = document.getElementById("notask")
 var singlecard = document.getElementById("singlecard")
 var mainheading = document.getElementById("mainheading")
-var navbar=document.getElementById("navbar")
+var navbar = document.getElementById("navbar")
 
 var issinglecard = false
 //add task button
 function showaddtask() {
-  addtaskpop.classList.remove('hide')
-  parent.classList.add('blur')
+    addtaskpop.classList.remove('hide')
+    parent.classList.add('blur')
 }
 
 function hideaddtask() {
-  addtaskpop.classList.add('hide')
-  parent.classList.remove('blur')
+    addtaskpop.classList.add('hide')
+    parent.classList.remove('blur')
 
 }
 
 function addcard() {
-   if(issinglecard){
-    back()
-    issinglecard=false
-   }
-   if(issinglecard){
-    back3()
-    issinglecard=false
-   }
-  notask.classList.add('hide')
-  hideaddtask()
-  // create card Element
-  let card = document.createElement("div")
-  let cardheading = document.createElement("h3")
-  let line = document.createElement("hr")
-  let itemlist = document.createElement("div")
-  let additem = document.createElement("button")
-  let deletebutton = document.createElement("button")
+    if (issinglecard) {
+        back()
+        issinglecard = false
+    }
+    if (issinglecard) {
+        back3()
+        issinglecard = false
+    }
+    notask.classList.add('hide')
+    hideaddtask()
+    // create card Element
+    let card = document.createElement("div")
+    let cardheading = document.createElement("h3")
+    let line = document.createElement("hr")
+    let itemlist = document.createElement("div")
+    let additem = document.createElement("button")
+    let deletebutton = document.createElement("button")
 
-  //append item 
-  cardcontainer.appendChild(card)
-  card.appendChild(cardheading)
-  card.appendChild(line)
-  card.appendChild(itemlist)
-  card.appendChild(additem)
-  card.appendChild(deletebutton)
+    //append item 
+    cardcontainer.appendChild(card)
+    card.appendChild(cardheading)
+    card.appendChild(line)
+    card.appendChild(itemlist)
+    card.appendChild(additem)
+    card.appendChild(deletebutton)
 
-  //give value to element
-  cardheading.innerText = cardname.value
-  cardname.value = ""
-  card.classList.add('card')
-  additem.innerText = "+"
-  deletebutton.innerText = "x"
-
-
-  // craete the class 
-  cardheading.classList.add('cardheading')
-  line.classList.add('line')
-  itemlist.classList.add('itemlist')
-  additem.classList.add('additem')
-  deletebutton.classList.add('deletebutton')
+    //give value to element
+    cardheading.innerText = cardname.value
+    cardname.value = ""
+    card.classList.add('card')
+    additem.innerText = "+"
+    deletebutton.innerText = "x"
 
 
-  //delete button
-  deletebutton.addEventListener('click', () => {
-    card.remove()
-    if (cardcontainer.innerText == "")
-      notask.classList.remove('hide')
-  })
-
-  //add item
-
-  additem.addEventListener('click', () => {
-    additempopup.classList.remove('hide')
-    parent.classList.add('blur')
-    //create item
-    let itempopheading = document.createElement("h3")
-    let itemname = document.createElement("input")
-    let addbutton = document.createElement("button")
-    let closebutton = document.createElement("button")
-
-    //append element
-    additempopup.appendChild(itempopheading)
-    additempopup.appendChild(itemname)
-    additempopup.appendChild(addbutton)
-    additempopup.appendChild(closebutton)
-
-    //give the value 
-    itempopheading.innerText = "Add task"
-    addbutton.innerText = "Add"
-    closebutton.innerText = "close"
-
-    // create a item class 
-    itempopheading.classList.add('itempopheading')
-    itemname.classList.add('itemname')
-    addbutton.classList.add('addbutton')
-    closebutton.classList.add('closebutton')
-    
+    // craete the class 
+    cardheading.classList.add('cardheading')
+    line.classList.add('line')
+    itemlist.classList.add('itemlist')
+    additem.classList.add('additem')
+    deletebutton.classList.add('deletebutton')
 
 
-    addbutton.addEventListener('click', () => {
-      parent.classList.remove('blur')
-      additempopup.classList.add('hide')
-      let item = document.createElement('div')
-      let itemtext = document.createElement('span')
-      let markdone = document.createElement('button')
-
-      
-      // appened child 
-      item.appendChild(itemtext)
-      item.appendChild(markdone)
-
-      //value
-      itemtext.innerText = itemname.value
-      markdone.innerText = "Mark Done"
-
-      //  appened  to item to item text
-      itemlist.appendChild(item)
-
-      additempopup.innerText = ""
-     // given the cklass
-     itemtext.classList.add('itemtext')
-     markdone.classList.add('markdone')
-    //  markdone button
-     markdone.addEventListener('click',()=>{
-      itemtext.classList.add('itemtextline')
-      markdone.classList.add('hide')
-      //itemtext button
-      itemtext.addEventListener('click',()=>{
-        itemtext.classList.remove('itemtextline')
-      markdone.classList.remove('hide')
-      })
-    })
+    //delete button
+    deletebutton.addEventListener('click', () => {
+        card.remove()
+        if (cardcontainer.innerText == "")
+            notask.classList.remove('hide')
     })
 
-    closebutton.addEventListener('click',()=> {
-      parent.classList.remove('blur')
-      additempopup.classList.add('hide')
-      addbutton.remove()
+    //add item
+
+    additem.addEventListener('click', () => {
+        additempopup.classList.remove('hide')
+        parent.classList.add('blur')
+        //create item
+        let itempopheading = document.createElement("h3")
+        let itemname = document.createElement("input")
+        let addbutton = document.createElement("button")
+        let closebutton = document.createElement("button")
+
+        //append element
+        additempopup.appendChild(itempopheading)
+        additempopup.appendChild(itemname)
+        additempopup.appendChild(addbutton)
+        additempopup.appendChild(closebutton)
+
+        //give the value 
+        itempopheading.innerText = "Add task"
+        addbutton.innerText = "Add"
+        closebutton.innerText = "close"
+
+        // create a item class 
+        itempopheading.classList.add('itempopheading')
+        itemname.classList.add('itemname')
+        addbutton.classList.add('addbutton')
+        closebutton.classList.add('closebutton')
+
+
+
+        addbutton.addEventListener('click', () => {
+            parent.classList.remove('blur')
+            additempopup.classList.add('hide')
+            let item = document.createElement('div')
+            let itemtext = document.createElement('span')
+            let markdone = document.createElement('button')
+
+
+            // appened child 
+            item.appendChild(itemtext)
+            item.appendChild(markdone)
+
+            //value
+            itemtext.innerText = itemname.value
+            markdone.innerText = "Mark Done"
+
+            //  appened  to item to item text
+            itemlist.appendChild(item)
+
+            additempopup.innerText = ""
+            // given the cklass
+            itemtext.classList.add('itemtext')
+            markdone.classList.add('markdone')
+            //  markdone button
+            markdone.addEventListener('click', () => {
+                itemtext.classList.add('itemtextline')
+                markdone.classList.add('hide')
+                //itemtext button
+                itemtext.addEventListener('click', () => {
+                    itemtext.classList.remove('itemtextline')
+                    markdone.classList.remove('hide')
+                })
+            })
+        })
+
+        closebutton.addEventListener('click', () => {
+            parent.classList.remove('blur')
+            additempopup.classList.add('hide')
+            addbutton.remove()
+        })
+
     })
-    
-  })
 
 
-  cardheading.addEventListener('click', ()=> {
-    issinglecard=true
-    navbar.classList.add('hide')
-    mainheading.classList.add('hide')
-    singlecard.classList.remove('hide')
-    cardcontainer.classList.add('hide')
-    let copycard = card.cloneNode(true)
-    singlecard.appendChild(copycard)
-    copycard.lastElementChild.addEventListener('click', () => {
-      card.remove()
-      copycard.remove()
-      back().remove
-      if (cardcontainer.innerText == "")
-        notask.classList.remove('hide')
-      
+    cardheading.addEventListener('click', () => {
+        issinglecard = true
+        navbar.classList.add('hide')
+        mainheading.classList.add('hide')
+        singlecard.classList.remove('hide')
+        cardcontainer.classList.add('hide')
+        let copycardheading = cardheading.cloneNode(true)
+        singlecard.appendChild(copycardheading)
+        copycardheading.classList.add('copycardheading')
+        let copycard = card.cloneNode(true)
+        singlecard.appendChild(copycard)
+        copycard.classList.add('copycard')
+        copycard.lastElementChild.addEventListener('click', () => {
+            card.remove()
+            copycard.remove()
+            // back().remove
+            if (cardcontainer.innerText == "")
+                notask.classList.remove('hide')
+
+        })
+        copycard.lastElementChild.previousElementSibling.addEventListener('click', () => {
+
+            additempopup.classList.remove('hide')
+            parent.classList.add('blur')
+            //create item
+            let itempopheading = document.createElement("h3")
+            let itemname = document.createElement("input")
+            let addbutton = document.createElement("button")
+            let closebutton = document.createElement("button")
+
+            //append element
+            additempopup.appendChild(itempopheading)
+            additempopup.appendChild(itemname)
+            additempopup.appendChild(addbutton)
+            additempopup.appendChild(closebutton)
+
+            //give the value 
+            itempopheading.innerText = "Add task"
+            addbutton.innerText = "Add"
+            closebutton.innerText = "close"
+
+            // create a item class 
+            itempopheading.classList.add('itempopheading')
+            itemname.classList.add('itemname')
+            addbutton.classList.add('addbutton')
+            closebutton.classList.add('closebutton')
+
+
+
+            addbutton.addEventListener('click', () => {
+                parent.classList.remove('blur')
+                additempopup.classList.add('hide')
+                let item = document.createElement('div')
+                let itemtext = document.createElement('span')
+                let markdone = document.createElement('button')
+
+
+                // appened child 
+                item.appendChild(itemtext)
+                item.appendChild(markdone)
+
+                //value
+                itemtext.innerText = itemname.value
+                markdone.innerText = "Mark Done"
+
+                //  appened  to item to item text
+                itemlist.appendChild(item)
+
+                additempopup.innerText = ""
+
+
+                // if (issinglecard) {
+                //     let copyitem= item.cloneNode(true);
+                //     copyitem.appendChild(copyitem);
+                //     copyaddbutton.addEventListener("click", function () {
+                //         itemtext.classList.add("markdone");
+                //         markdone.style.display = "none";
+                //     //     //create item clone
+                //     //     //append to copylist
+                //     //     //markdone eventlistner
+                //     })
+                // }
+                // given the cklass
+                itemtext.classList.add('itemtext')
+                markdone.classList.add('markdone')
+                //  markdone button
+                markdone.addEventListener('click', () => {
+                    itemtext.classList.add('itemtextline')
+                    markdone.classList.add('hide')
+                    //itemtext button
+                    itemtext.addEventListener('click', () => {
+                        itemtext.classList.remove('itemtextline')
+                        markdone.classList.remove('hide')
+                    })
+                })
+
+                if (issinglecard) {
+                    let copyitem= item.cloneNode(true);
+                    copyitem.appendChild(copyitem);
+                    copyaddbutton.addEventListener("click", function () {
+                        itemtext.classList.add("markdone");
+                        markdone.style.display = "none";
+                    //     //create item clone
+                    //     //append to copylist
+                    //     //markdone eventlistner
+                    })
+                }
+            })
+
+            closebutton.addEventListener('click', () => {
+                parent.classList.remove('blur')
+                additempopup.classList.add('hide')
+                addbutton.remove()
+            })
+
+
+            // parent.firstElementChild.classList.remove('hide')
+        })
+         
+        parent.firstElementChild.classList.remove('hide')
+
     })
-    parent.firstElementChild.classList.remove('hide')
-  })
-
 }
 
 function back() {
-  parent.firstElementChild.classList.add('hide')
-  singlecard.classList.add('hide')
-  mainheading.classList.remove('hide')
-  navbar.classList.remove('hide')
-  cardcontainer.classList.remove('hide')
-  singlecard.innerText = ""
+    parent.firstElementChild.classList.add('hide')
+    singlecard.classList.add('hide')
+    mainheading.classList.remove('hide')
+    navbar.classList.remove('hide')
+    cardcontainer.classList.remove('hide')
+    singlecard.innerText = ""
 }
 function back2() {
-  parent.firstElementChild.classList.add('hide')
-  singlecard.classList.add('hide')
-  mainheading.classList.remove('hide')
-  navbar.classList.remove('hide')
-  cardcontainer.classList.remove('hide')
-  singlecard.innerText = ""
+    parent.firstElementChild.classList.add('hide')
+    singlecard.classList.add('hide')
+    mainheading.classList.remove('hide')
+    navbar.classList.remove('hide')
+    cardcontainer.classList.remove('hide')
+    singlecard.innerText = ""
 }
